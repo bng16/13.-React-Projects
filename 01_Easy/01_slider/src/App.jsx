ce1317c66d734d76d593fdc97d2d2e36fa4f9fd3
bng16 <biswajitnag161619@gmail.com>
2024-08-10 00:55:54 +0530

    car arr randerd

--- 01_Easy/01_slider/src/App.jsx
+++ 01_Easy/01_slider/src/App.jsx
@@ -1,12 +1,33 @@
+import car1 from "./assets/cars/car1.png";
+import car2 from "./assets/cars/car2.png";
+import car3 from "./assets/cars/car3.png";
+import car4 from "./assets/cars/car4.png";
+import car5 from "./assets/cars/car5.png";
+import car6 from "./assets/cars/car6.png";
+import car7 from "./assets/cars/car7.png";
+import car8 from "./assets/cars/car8.png";
 
+
+import Slide from "./components/Slide";
 
 function App() {
 
+  const carLink=[
+    car1,
+    car2,
+    car3,
+    car4,
+    car5,
+    car6,
+    car7,
+    car8,
+  ]
+
 
   return (
-    <>
-      
-    </>
+    <div className='w-full h-screen bg-red-50 flex justify-center items-center'>
+      <Slide carLink={carLink}/>
+    </div>
   )
 }
 
---
+++ 01_Easy/01_slider/src/components/Slide.jsx
@@ -0,0 +1,11 @@
+function Slide({carLink}) {
+  return (
+    <div className='flex  w-[50vw] overflow-hidden'>
+      {carLink.map((imglink)=><img src={imglink} className='w-[100vw]'/>)}
+    </div>
+  )
+}
+
+export default Slide
+
+
