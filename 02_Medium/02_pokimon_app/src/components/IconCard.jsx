16892b7cb1dd1dfbfd4bef6aedb3d3b8d5cc4208
bng16 <biswajitnag161619@gmail.com>
2024-09-17 22:22:48 +0530

    same code but with context api

--- 02_Medium/02_pokimon_app/src/components/IconCard.jsx
+++ 02_Medium/02_pokimon_app/src/components/IconCard.jsx
@@ -16,33 +16,3 @@ function IconCard({ pokemon }) {
 }
 
 export default IconCard;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-
-// function IconCard({pokemon,setSelectedPokemon}) {
-
-//   return (
-//     <div className=' w-[10vw] h-[10vw] rounded-lg flex flex-col  border  hover:border-black ease-in-out duration-300 cursor-pointer' onClick={()=>setSelectedPokemon(pokemon)}>
-//       <div className='w-full h-1/2 '></div>
-//       <div className='relative w-full h-1/2 bg-gray-100 rounded-b-lg flex justify-center items-center'>
-//         <h1>{pokemon.name}</h1>
-//         <img src={pokemon.sprites.front_default} alt=""  className='absolute w-[60%] top-[-15%] left-[50%] translate-x-[-50%] translate-y-[-50%]'/>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default IconCard
